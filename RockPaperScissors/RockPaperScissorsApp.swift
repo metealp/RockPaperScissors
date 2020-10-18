b386756ced0ba1a9f69f0c3be51ba3e5e31bfca6
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-18 15:09:37 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Mete Alp Kizilcay on 18.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
